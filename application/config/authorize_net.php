7890d08eb58557c8826481fb08b40e682650cd30
Daniel Gabris <softabroad71@outlook.com>
2016-05-27 06:05:08 +0200

    turn on production in authorize.net

--- application/config/authorize_net.php
+++ application/config/authorize_net.php
@@ -1,9 +1,9 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // Authorize.net Account Info
-$config['api_login_id'] = '8ZsdWGPp76n';
-$config['api_transaction_key'] = '7VMa67y4u8t8U92A';
-$config['api_url'] = 'https://test.authorize.net/gateway/transact.dll'; // TEST URL
+$config['api_login_id'] = '3Sbmm39Aa';
+$config['api_transaction_key'] = '7nKE69P3yf3t96B5';
+$config['api_url'] = 'https://secure.authorize.net/gateway/transact.dll'; // TEST URL
 //$config['api_url'] = 'https://secure.authorize.net/gateway/transact.dll'; // PRODUCTION URL
 
 /* EOF */
