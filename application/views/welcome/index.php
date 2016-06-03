fc78a4b1a6cd53effd704e30ae93757be632fb83
Daniel Gabris <softabroad71@outlook.com>
2016-06-03 10:26:59 +0200

    added live counter

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -3,24 +3,27 @@
   <h3>Photo Shoot Competition Application</h3>
 
   <div class="live-counter">
-    <p id="live_counter_detail"></p>
+    <p id="live_counter_detail">Current grand prize is ----</p>
   </div>
   <script>
-    $(function () {
-      initializeCounter("<?php echo site_url();?>");
+    jQuery(document).ready(function () {
+      initializeCounter();
     });
-    function initializeCounter($base_path) {
+    function initializeCounter() {
       function numberWithCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
       }
       var poller = function() {
-        $.get('http://www.deangelomotors.com/app/index.php/live/counter', function(response) {
+        jQuery.get('http://www.deangelomotors.com/app/index.php/live/counter', function(response) {
           $('#live_counter_detail').text('Current grand prize is $'+numberWithCommas(response));
         });
       };
-      setInterval(poller, 2000);
+      poller();
+      setInterval(poller, 5000);
     }
   </script>
+
+
 
   <div class="field">
     <p>You will need to submit payment to enter contest.</p>
