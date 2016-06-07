8d20a2372c5fde76f7274cc317c7b661d1cce449
Daniel Gabris <softabroad71@outlook.com>
2016-06-07 12:03:02 +0200

    minor style

--- application/config/authorize_net.example.php
+++ application/config/authorize_net.example.php
@@ -1,8 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 // Authorize.net Account Info
-$config['api_login_id'] = '8ZsdWGPp76n';
-$config['api_transaction_key'] = '7VMa67y4u8t8U92A';
+$config['api_login_id'] = 'xxxxxxxxxxxx';
+$config['api_transaction_key'] = 'xxxxxxxxxxxxxxxx';
 $config['api_url'] = 'https://test.authorize.net/gateway/transact.dll'; // TEST URL
 //$config['api_url'] = 'https://secure2.authorize.net/gateway/transact.dll'; // PRODUCTION URL
 
